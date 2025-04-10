cc52ddc28b0040c723a4016424bed090ea945428
João Guilherme Bottura Ramos <109831190+jguigas2@users.noreply.github.com>
2025-04-10 20:38:21 -0300

    Implementação da camada de domínio

--- src/main/java/me/dio/projeto/springuigas/domain/Application.java
+++ src/main/java/me/dio/projeto/springuigas/domain/Application.java
@@ -1,4 +1,4 @@
-package me.dio.projeto.springuigas;
+package me.dio.projeto.springuigas.domain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
